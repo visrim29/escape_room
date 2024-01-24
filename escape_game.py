a52619a04762deb26b301c0349b55e1ee5adfac3
visrim29 <r.visagurskis@gmail.com>
2024-01-24 13:44:42 +0200

    4-ta užduotis

--- escape_game.py
+++ escape_game.py
@@ -17,6 +17,7 @@ layout_one = [
     [sg.Text(key='OUTPUT_ONE')]
 ]
 
+
 layout_two = [
     [sg.Text('ANTRAS LYGIS')],
     [sg.Text('?somargorp ši itieši etiron radsiv rA')],
@@ -31,6 +32,7 @@ layout_two = [
     [sg.Text(key='OUTPUT_TWO')]
 ]
 
+
 layout_three = [
     [sg.Text('III LYGIS')],
     [sg.Text('Sveikiname, pasiekėte III-ią lygį!')],
@@ -44,15 +46,26 @@ layout_three = [
     [sg.Text(key='OUTPUT_THREE')]
 ]
 
+# Nekeisti esamo sg.Text ir sg.Input formato
+layout_four = [
+    [sg.Text('KETVIRTAS LYGIS - NEMATOMAS RAŠALAS')],    
+    [sg.Text('\n\nUžduotis:\nĮveskite skaičius: 1337\nĮvedę skaičius spauskite mygtuką', justification='center', size=(40, 7), text_color='#fffff1', background_color='#ffffff')],
+    [sg.Text('Jūsų įvestis:')],
+    [sg.Input(key='INPUT_FOUR', size=(43, 1))],
+    [
+    sg.Button('', key='MYGTUKAS', size=(20, 2)),
+    sg.Button('Tęsti', key='CONTINUE_FOUR', visible=False)
+    ],
+    [sg.Text(key='OUTPUT_FOUR')]
+]
+
 level_one = sg.Window('Escape: PIRMAS LYGIS', layout_one, font='Arial 20')
 
 level_two = sg.Window('Escape: ANTRAS LYGIS', layout_two, font='Arial 20')
 
 level_three = sg.Window('Escape: III LYGIS', layout_three, font='Arial 20')
 
-# Parašyti layout ir užpildyti parametrus
-# level_four = sg.Window('')
-
+level_four = sg.Window('Escape: KETVIRTAS LYGIS - NEMATOMAS RAŠALAS', layout_four, font='Arial 20')
 
 
 if level_one:
@@ -92,7 +105,7 @@ if level_two:
         if event_2 == 'CONTINUE_TWO':
             level_two.close()
 
-
+# Padaryti ne case sensitive, išskyrus atsakymą
 if level_three:
     while True:
         event_3, values_3 = level_three.read()
@@ -113,11 +126,23 @@ if level_three:
         elif event_3 == 'GUESS' and values_3['INPUT_THREE'] == atsakymas:
             level_three['OUTPUT_THREE'].update('Pasirinkimas teisingas, spauskite mygtuką "Tęsti"')
             level_three['CONTINUE_THREE'](visible=True)
-            level_three.close()
-# Parašyti level_four            
-#           level_four.read()
+            level_three.close()            
+            level_four.read()
 
 
-level_one.close()
-level_two.close()
-level_three.close()
+if level_four:
+    while True:
+        event_4, values_4 = level_four.read()
+        if event_4 == sg.WIN_CLOSED:
+            break
+        elif event_4 == 'MYGTUKAS' and values_4['INPUT_FOUR'] != '1337':
+            level_four['OUTPUT_FOUR'].update('Užduotis neįvykdyta, bandykite dar kartą')
+        elif event_4 == 'MYGTUKAS' and values_4['INPUT_FOUR'] == '1337':
+            level_four['OUTPUT_FOUR'].update('Užduotis įvykdyta, spauskite mygtuką "Tęsti"')
+            level_four['CONTINUE_FOUR'](visible=True)
+#             level_four.close()
+
+
+# level_one.close()
+# level_two.close()
+# level_three.close()
